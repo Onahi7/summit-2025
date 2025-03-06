2c571842e86c3931fcb06d372df0443a3a9e6720
Onahi7 <dicksonhardy7@gmail.com>
2025-03-06 20:09:39 +0100

    updates

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-nesting'),
-    require('@tailwindcss/postcss7-compat'),
+    require('tailwindcss'),
     require('autoprefixer')
   ]
 }
